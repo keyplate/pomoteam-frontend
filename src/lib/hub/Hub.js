589325e78e4af17ba105c3ef58ae02b9e44eaf2c
Kyrylo Lapchenko <kyrylo.lap@gmail.com>
2025-03-28 20:51:38 +0100

    Clean up

--- src/lib/hub/Hub.js
+++ src/lib/hub/Hub.js
@@ -3,9 +3,14 @@ import { ERROR_MESSAGES } from '$lib/hub/errors/hubError.js';
 import { ERROR_TYPES } from '$lib/hub/errors/hubError.js';
 import { env } from '$env/dynamic/public';
 
+/**
+ * @file - manages hub lifecycle
+ */
+
 const HUB_ENDPOINT = env.PUBLIC_HUB_ENDPOINT;
 
 /**
+ * @description - creates a hub. Hub represents a shared space for collaboration which syncs state of the app between users.
  * @param {string} hubId
  * @return {Promise<string>}
  */
@@ -21,12 +26,6 @@ export async function createHub(hubId) {
     });
 
     if (!response.ok) {
-        if (response.status === 409) { // Example status code for conflict
-            throw new HubOperationError(
-                ERROR_MESSAGES[ERROR_TYPES.ALIAS_CONFLICT],
-                ERROR_TYPES.ALIAS_CONFLICT
-            );
-        }
         throw new HubOperationError(
             ERROR_MESSAGES[ERROR_TYPES.SERVER],
             ERROR_TYPES.SERVER
@@ -37,6 +36,12 @@ export async function createHub(hubId) {
     return data.id;
 }
 
+/**
+ * @description - checks if hub exists to prevent websocket connection to a nonexistent hub.
+ * @arg {string} hubId
+ * @arg {function} fetch
+ * @return {Promise<boolean>}
+ */
 export async function checkHubExists(hubId, fetch) {
     if (!hubId) {
         throw new HubOperationError(
@@ -56,52 +61,6 @@ export async function checkHubExists(hubId, fetch) {
         return response.status === 200;
 
     } catch (error) {
-        if (error instanceof HubOperationError) {
-            throw error;
-        }
-        throw new HubOperationError(
-            ERROR_MESSAGES[ERROR_TYPES.NETWORK],
-            ERROR_TYPES.NETWORK
-        );
-    }
-}
-
-export async function deleteRoom(hubId) {
-    if (!hubId) {
-        throw new HubOperationError(
-            ERROR_MESSAGES[ERROR_TYPES.INVALID_HUB_ID],
-            ERROR_TYPES.INVALID_HUB_ID
-        );
-    }
-
-    try {
-        const response = await fetch(`${HUB_ENDPOINT}/${hubId}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        // Handle specific status codes
-        switch (response.status) {
-            case 200:
-            case 204:
-                return true; // Successfully deleted
-            case 404:
-                throw new HubOperationError(
-                    ERROR_MESSAGES[ERROR_TYPES.HUB_NOT_FOUND],
-                    ERROR_TYPES.HUB_NOT_FOUND
-                );
-            default:
-                throw new HubOperationError(
-                    ERROR_MESSAGES[ERROR_TYPES.SERVER],
-                    ERROR_TYPES.SERVER
-                );
-        }
-    } catch (error) {
-        if (error instanceof HubOperationError) {
-            throw error;
-        }
         throw new HubOperationError(
             ERROR_MESSAGES[ERROR_TYPES.NETWORK],
             ERROR_TYPES.NETWORK
